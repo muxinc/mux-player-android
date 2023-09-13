63c3f90df2273decc0c90d2e970e0b18b67744b6
Emily Dixon <edixon@mux.com>
2023-09-13 13:23:15 -0700

    Reformat and other stuff

--- app/src/main/java/com/mux/video/media3/MainActivity.kt
+++ app/src/main/java/com/mux/video/media3/MainActivity.kt
@@ -6,13 +6,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.mux.video.media3.databinding.ActivityMainBinding
 import com.mux.video.media3.databinding.ListitemExampleBinding
+import com.mux.video.media3.examples.BasicPlayerActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,19 +27,16 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun setUpExampleList() {
-    val exampleAdapter = ExampleListAdapter(this, EXAMPLES)
+    val exampleAdapter = ExampleListAdapter(this, examples)
     examplesView.adapter = exampleAdapter
   }
 
-  companion object {
-    val EXAMPLES = listOf(
-      Example(
-        title = "placeholder",
-        destination = Intent()
-      )
+  private val examples = listOf(
+    Example(
+      title = "Basic Foreground Player",
+      destination = Intent(this@, BasicPlayerActivity::class.java)
     )
-  }
-
+  )
 }
 
 data class Example(
