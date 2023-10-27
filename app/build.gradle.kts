6ab834be6b9bed9491e18a3eeca35f6a290a779e
Emily Dixon <edixon@mux.com>
2023-10-27 12:22:03 -0700

    update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,9 @@ android {
 dependencies {
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("com.google.android.material:material:1.9.0")
+  implementation("com.google.android.material:material:1.10.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-  implementation("androidx.recyclerview:recyclerview:1.3.1")
+  implementation("androidx.recyclerview:recyclerview:1.3.2")
   implementation(project(":library"))
 
   testImplementation("junit:junit:4.13.2")
