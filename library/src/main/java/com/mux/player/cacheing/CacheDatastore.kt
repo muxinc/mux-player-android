af92ecb11e02df71383175be97a11c6a5272f20d
Emily Dixon <edixon@mux.com>
2024-01-22 09:15:17 -0800

    correct a comment

--- library/src/main/java/com/mux/player/cacheing/CacheDatastore.kt
+++ library/src/main/java/com/mux/player/cacheing/CacheDatastore.kt
@@ -22,7 +22,7 @@ class CacheDatastore(val context: Context) {
 
 private class DbHelper(appContext: Context) : SQLiteOpenHelper(
   /*context = */ appContext,
-  /* name */DB_FILE, // todo - put file in `cacheDir/mux/player/` or something
+  /* name */DB_FILE, // todo - put file in `filesDir/mux/player/` or something
   null,
   DB_VERSION
 ) {
