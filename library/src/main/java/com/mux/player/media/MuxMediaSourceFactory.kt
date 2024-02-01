132a1e273003bf9e5b915cd5ab0d0bc9626b4847
Emily Dixon <edixon@mux.com>
2024-02-01 11:09:10 -0800

    better doc

--- library/src/main/java/com/mux/player/media/MuxMediaSourceFactory.kt
+++ library/src/main/java/com/mux/player/media/MuxMediaSourceFactory.kt
@@ -10,8 +10,15 @@ import androidx.media3.exoplayer.source.MediaSource
 import androidx.media3.exoplayer.upstream.CmcdConfiguration
 
 /**
- * A [MediaSource.Factory] configured to work best with Mux Video. Prefer to use this over
- * [DefaultMediaSourceFactory] so we can provide our own defaults (turning on CMCD, caching, etc)
+ * A [MediaSource.Factory] configured to work best with Mux Video.
+ *
+ * If possible, you should prefer to allow us to manage the MediaSourceFactory by using the defaults
+ * on `MuxPlayer.Builder`. Also prefer to use this over [DefaultMediaSourceFactory] so we can
+ * provide our own defaults (turning on CMCD, caching, etc)
+ *
+ * We also provide our own `DataSource.Factory`, which is a `DefaultMediaSourceFactory` that also
+ * delegates to our disk caching layer. You can override it with the `dataSourceFactory` ctor param,
+ * but caching is disabled by default so you don't need to worry about it if you don't want caching
  *
  * If you wish to inject your own `DefaultMediaSourceFactory` then its `DataSource.Factory` will be
  * superseded by Mux's custom one. To override that, you can provide your own value for
--- library/src/test/java/com/mux/player/MuxDataSourceTests.kt
+++ library/src/test/java/com/mux/player/MuxDataSourceTests.kt
@@ -1,9 +1,22 @@
 package com.mux.player
 
+import android.content.Context
+import androidx.media3.datasource.DataSource
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import com.mux.player.media.MuxDataSource
+import com.mux.player.media.MuxMediaSourceFactory
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Test
 
 class MuxDataSourceTests : AbsRobolectricTest() {
   @Test
   fun `open() should change URLs to go to the Proxy`() {
+    // todo - object under test (MuxDataSource) returned by `MuxDataSource.Factory` injected to ctor
+    val mockContext = mockk<Context>()
+    val muxMediaSourceFactory = MuxMediaSourceFactory(
+      ctx = mockContext,
+      innerFactory = DefaultMediaSourceFactory(mockContext),
+    )
   }
 }
