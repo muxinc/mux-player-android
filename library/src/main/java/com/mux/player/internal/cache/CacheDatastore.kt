a289e9c9c632ec8f5a4fcc43467b6805e7dfa460
Emily Dixon <edixon@mux.com>
2024-02-16 14:25:14 -0800

    Revert "Make CacheDatastore public"
    This reverts commit b6127cde0a83bc6f9c12e2e20786f2f0f8e58339.

--- library/src/main/java/com/mux/player/internal/cache/CacheDatastore.kt
+++ library/src/main/java/com/mux/player/internal/cache/CacheDatastore.kt
@@ -19,7 +19,7 @@ import java.util.concurrent.atomic.AtomicReference
  * reading and writing from the cache, as well as methods for obtaining files for the Proxy to
  * download into.
  *
- * The SDK should keep an instance of this class open as long as the cache could likely be accessed.
+ * You should keep an instance of this class open as long as the cache could likely be accessed.
  * CacheController is immediately responsible for deciding this (though in this in-dev iteration it
  * simply keeps one and doesn't close it, which we should change before 1.0)
  */
