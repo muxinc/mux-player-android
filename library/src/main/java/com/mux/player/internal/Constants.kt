383dfea6a712b47d9dede52efe0b9b7a0de12b2f
Emily Dixon <edixon@mux.com>
2024-04-23 12:33:09 -0700

    duplicate key

--- library/src/main/java/com/mux/player/internal/Constants.kt
+++ library/src/main/java/com/mux/player/internal/Constants.kt
@@ -2,5 +2,5 @@ package com.mux.player.internal
 
 internal object Constants {
   const val BUNDLE_DRM_TOKEN = "drm key"
-  const val BUNDLE_PLAYBACK_ID = "drm key"
+  const val BUNDLE_PLAYBACK_ID = "playback id"
 }
