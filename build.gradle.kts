48ef3291ef8f4d6a9c46f730bc7550bcdc3fb247
Emily Dixon <edixon@mux.com>
2024-08-23 17:41:34 -0700

    Update the library plugin too

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("com.android.application") version "8.5.2" apply false
   id("org.jetbrains.kotlin.android") version "2.0.10" apply false
-  id("com.android.library") version "8.1.4" apply false
+  id("com.android.library") version "8.5.2" apply false
   id("com.mux.gradle.android.mux-android-distribution") version "1.2.1" apply false
 }
 
