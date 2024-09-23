1b39c2764a064a4d837041753e28ab44a8493390
Emily Dixon <edixon@mux.com>
2024-09-23 14:06:44 -0700

    Update to media3 1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
   id("com.android.application") version "8.5.2" apply false
   id("org.jetbrains.kotlin.android") version "2.0.10" apply false
   id("com.android.library") version "8.5.2" apply false
-  id("com.mux.gradle.android.mux-android-distribution") version "1.2.1" apply false
+  id("com.mux.gradle.android.mux-android-distribution") version "1.3.0" apply false
 }
 
 allprojects {
   ext {
-    set("muxDataVersion", "1.1.0")
+    set("muxDataVersion", "1.5.2")
   }
 }
