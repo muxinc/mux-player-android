a4daada990058845e55cd895b0e04947d8bc0052
Emily Dixon <edixon@mux.com>
2024-09-26 14:11:17 -0700

    Now it's this

---
+++ .sauce/generate-config.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+branch_name="${BUILDKITE_BRANCH}"
+if [ -z "$branch_name" ]; then
+    branch_name=$(git branch)
+fi
+export branch_name
+commit="$(git rev-parse --short HEAD)"
+export commit
+
+sed -E \
+  -e "s/TEMPLATE_BRANCH/${BUILDKITE_BRANCH}/g" \
+  -e "s/TEMPLATE_COMMIT/${BUILDKITE_BRANCH}/g" \
+  .sauce/template.sauce-data.yml > .sauce/conf.yml
+
+sed -E \
+  -e "s/TEMPLATE_BRANCH/${BUILDKITE_BRANCH}/g" \
+  -e "s/TEMPLATE_COMMIT/${BUILDKITE_BRANCH}/g" \
+  .sauce/template.sauce-player.yml > .sauce/conf.yml
