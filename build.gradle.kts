c4d065939f4181cd6e8d7042747cfdc7003084d1
Emily Dixon <edixon@mux.com>
2024-11-08 12:10:28 -0800

    Update tools + kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("com.android.application") version "8.5.2" apply false
-  id("org.jetbrains.kotlin.android") version "2.0.10" apply false
-  id("com.android.library") version "8.5.2" apply false
-  id("com.mux.gradle.android.mux-android-distribution") version "1.2.1" apply false
+  id("com.android.application") version "8.7.2" apply false
+  id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+  id("com.android.library") version "8.7.2" apply false
+  id("com.mux.gradle.android.mux-android-distribution") version "1.3.0" apply false
 }
 
 allprojects {
