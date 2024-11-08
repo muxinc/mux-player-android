a869acdc8ff03cd28900e43bba312658c6c5a57b
Emily Dixon <edixon@mux.com>
2024-11-08 12:06:40 -0800

    Update media3 to 1.4.1 + mux data to 1.6.0 (#70)
    * Update media3 + mux data
    
    * automatedtests don't even need that dep
    
    * automated tests should usually fail if there's playback errors
    
    * Maybe 1 retry would be enough
    
    * Ignore livestream metrics test until we have an available stream
    
    * turn the test back on

--- automatedtests/src/androidTest/java/com/mux/player/media3/automatedtests/LatencyTests.java
+++ automatedtests/src/androidTest/java/com/mux/player/media3/automatedtests/LatencyTests.java
@@ -8,6 +8,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class LatencyTests extends TestBase {
