088d2d87cb5d0b4d083c69f262bb7b83d47599d9
Emily Dixon <edixon@mux.com>
2024-12-17 13:14:45 -0800

    Releases/v1.3.0 (#73)
    ## Improvements
    
    * chore(dependencies): Update Media3 to 1.5.0 (#72)
    
    
    
    Co-authored-by: Ryan McClarnon <ryan@joinblink.com>
    Co-authored-by: GitHub <noreply@github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-  id("com.android.application") version "8.7.2" apply false
+  id("com.android.application") version "8.7.3" apply false
   id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-  id("com.android.library") version "8.7.2" apply false
+  id("com.android.library") version "8.7.3" apply false
   id("com.mux.gradle.android.mux-android-distribution") version "1.3.0" apply false
 }
 
 allprojects {
   ext {
-    set("muxDataVersion", "1.6.0")
+    set("muxDataVersion", "1.6.2")
   }
 }
