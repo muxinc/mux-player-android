263f8c76c7c4146e4cdc659706d1b4502d86efb1
Emily Dixon <edixon@mux.com>
2025-04-08 09:56:47 -0700

    try 1.7.3 again

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("com.android.application") version "8.8.2" apply false
-  id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+  id("org.jetbrains.kotlin.android") version "2.1.20" apply false
   id("com.android.library") version "8.8.2" apply false
   id("com.mux.gradle.android.mux-android-distribution") version "1.3.0" apply false
 }
