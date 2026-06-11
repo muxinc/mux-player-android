d0e3d91335dd9ff9f9da443c67d1f1826224107c
Emily Dixon <edixon@mux.com>
2026-06-10 19:32:48 -0700

    back to sdk 36 for now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
   namespace = "com.mux.player.media3"
-  compileSdk = 37
+  compileSdk = 36
 
   defaultConfig {
     minSdk = 23
     //noinspection EditedTargetSdkVersion
-    targetSdk = 37
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
 
