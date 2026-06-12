79f5ff2e87e6cffc76c0fada1de0528ad4f70103
Emily Dixon <edixon@mux.com>
2026-06-12 14:06:56 -0700

    new ver

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
   id("com.android.application") version "9.2.1" apply false
 //  id("org.jetbrains.kotlin.android") version "2.2.10" apply false
   id("com.android.library") version "9.2.1" apply false
-  id("com.mux.gradle.android.mux-android-distribution") version "1.4.0-ng3" apply false
+  id("com.mux.gradle.android.mux-android-distribution") version "1.4.0-ng4" apply false
 }
