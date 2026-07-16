ca7726f192f198f099b1b4e8d09b36b374a2a158
Emily Dixon <edixon@mux.com>
2026-07-16 13:18:00 -0700

    Very nearly a complete download test

--- library/src/androidTest/java/com/mux/player/OfflineDownloadInstrumentationTests.kt
+++ library/src/androidTest/java/com/mux/player/OfflineDownloadInstrumentationTests.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.media.MediaDrm
+import android.util.Log
 import androidx.media3.common.MediaItem
 import androidx.media3.database.DatabaseProvider
 import androidx.media3.database.DefaultDatabaseProvider
@@ -12,8 +13,11 @@ import androidx.media3.datasource.cache.NoOpCacheEvictor
 import androidx.media3.datasource.cache.SimpleCache
 import androidx.media3.exoplayer.drm.DrmSessionManager
 import androidx.media3.exoplayer.offline.DefaultDownloadIndex
+import androidx.media3.exoplayer.offline.Download
 import androidx.media3.exoplayer.offline.DownloadIndex
 import androidx.media3.exoplayer.offline.DownloadManager
+import androidx.media3.exoplayer.offline.DownloadRequest
+import androidx.media3.exoplayer.scheduler.Requirements
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.mux.player.internal.createLogcatLogger
@@ -24,12 +28,14 @@ import com.mux.player.media.MuxDrmSessionManagerProvider
 import com.mux.player.offline.MuxHlsDownloadCallback
 import com.mux.player.offline.MuxOfflineCmafHlsMediaSource
 import com.mux.player.offline.createMuxHlsDownloadHelper
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.File
+import java.lang.Exception
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
@@ -68,6 +74,7 @@ class OfflineDownloadInstrumentationTests {
       /*upstreamFactory=*/ DefaultHttpDataSource.Factory(),
       /*executor=*/ ioExecutor
     )
+    mediaDownloadManager.requirements = Requirements(0) // no requirements for starting
   }
 
   @After
@@ -85,6 +92,8 @@ class OfflineDownloadInstrumentationTests {
       drmSessionManagerProvider = clearTextDrmSessionManagerProvider
     )
 
+    val preparationComplete = CompletableDeferred<DownloadRequest>()
+
     val downloadHelper = createMuxHlsDownloadHelper(appContext, fileMediaSource)
     downloadHelper.prepare(MuxHlsDownloadCallback(
       fileMediaSource,
@@ -96,9 +105,61 @@ class OfflineDownloadInstrumentationTests {
       playbackId = mediaItem.getPlaybackId()!!, // MediaItems.fromMuxPlaybackId tested elsewhere
       drmToken = null,
       ioExecutor = ioExecutor,
-      onReady = { },
-      onError = { }
+      onReady = { preparationComplete.complete(it) },
+      onError = { preparationComplete.completeExceptionally(it) }
     ))
+
+    // Throws here if there was an error selecting tracks
+    val downloadRequest = preparationComplete.await()
+
+    val downloadComplete = CompletableDeferred<Download>()
+    mediaDownloadManager.addListener(object : DownloadManager.Listener {
+      override fun onDownloadChanged(
+        downloadManager: DownloadManager,
+        download: Download,
+        finalException: Exception?
+      ) {
+        Log.d(logTag(), "download state changed:\n\tstate:${download.state}")
+        when (download.state) {
+          Download.STATE_COMPLETED -> {
+            Log.d(logTag(), "download complete")
+            downloadComplete.complete(download)
+          }
+          Download.STATE_DOWNLOADING -> {
+            Log.d(logTag(), "downloading: ${download.percentDownloaded}% (${download.bytesDownloaded} bytes)")
+          }
+          Download.STATE_FAILED -> {
+            // contract guarantees finalException when state is failed
+            downloadComplete.completeExceptionally(
+              finalException ?: AssertionError("failed without exception")
+            )
+          }
+        }
+      }
+
+      override fun onInitialized(downloadManager: DownloadManager) {
+        Log.v(logTag(), "DownloadManager onInitialized()1")
+      }
+
+      override fun onRequirementsStateChanged(
+        downloadManager: DownloadManager,
+        requirements: Requirements,
+        notMetRequirements: Int
+      ) {
+        if (notMetRequirements != 0) {
+          // for our test this isn't expected to happen, but the test shouldn't hang if it does
+          downloadComplete.completeExceptionally(
+            AssertionError("DownloadManager didn't want to start. Requirements too strict?")
+          )
+        }
+      }
+    })
+
+    mediaDownloadManager.addDownload(downloadRequest)
+
+    val completedDownload = downloadComplete.await()
+
+    // TODO: Assert on this completed download
   }
 
   @Test
